90a9ec02a4ccbf25ea38cfcb2bdfb0869857e506
Charles <charles.melo145@gmail.com>
2024-10-07 17:41:55 -0300

    criando models

---
+++ Models/TiketModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ticketApi.Models
+{
+    public class TiketModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool status { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
