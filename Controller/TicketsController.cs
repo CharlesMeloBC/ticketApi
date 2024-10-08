0a1c64e6a9052f434c8469e3527b67a4acef3109
Charles <charles.melo145@gmail.com>
2024-10-07 21:01:19 -0300

    add endpoints

---
+++ Controller/TicketsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ticketApi.Data;
+using ticketApi.Models;
+
+
+namespace ticketApi.Controller
+{
+    [Route("api[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TicketsController (AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TiketModel>>> GetAllTickets()
+        {
+            var tickets = await _context.Tikets.ToListAsync();
+            return Ok(tickets);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TiketModel>> GetTicketById(int id)
+        {
+            var tickets = await _context.Tikets.FindAsync(id);
+            if (tickets == null) 
+                return NotFound();
+            return Ok(tickets);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TiketModel>> CreateTicket([FromBody] TiketModel ticket)
+        {
+            if (ticket == null || string.IsNullOrEmpty(ticket.Name))
+                return BadRequest("Você precisa preencher os campos");
+            _context.Tikets.Add(ticket);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(CreateTicket), new {id = ticket.Id }, ticket);
+
+        }
+    }
+}
---
+++ Migrations/20241008000024_TicketsEndPoints.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ticketApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class TicketsEndPoints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
